7250eff2dec24794daefd508ab1bea34d3b95075
taxilng <769967440@qq.com>
2020-11-09 19:43:19 +0800

    增加了桌面，移动端的CPU和GPU天梯图

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js',
+        cpu: './src/empty.js',
+        gpu: './src/empty.js',
+        mcpu: './src/empty.js',
+        mgpu: './src/empty.js',
+    },
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist'
     },
     output: {
-        filename: 'bundle.[hash].js',
+        filename: '[name].bundle.[hash].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -64,12 +70,66 @@ module.exports = {
             filename: 'index.html',
             template: './src/mobile.html',
             hash: true,
+            favicon: path.resolve('favicon.ico'),
+            chunks: ['main'],
             minify: {
                 // 压缩HTML文件
                 removeComments: true, // 移除HTML中的注释
                 collapseWhitespace: true, // 删除空白符与换行符
                 minifyCSS: true // 压缩内联css
             }
-        })
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'desktop_cpu.html',
+            template: './src/other/desktop_cpu.html',
+            hash: true,
+            favicon: path.resolve('favicon.ico'),
+            chunks: ['cpu'],
+            minify: {
+                // 压缩HTML文件
+                removeComments: true, // 移除HTML中的注释
+                collapseWhitespace: true, // 删除空白符与换行符
+                minifyCSS: true // 压缩内联css
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'desktop_gpu.html',
+            template: './src/other/desktop_gpu.html',
+            hash: true,
+            favicon: path.resolve('favicon.ico'),
+            chunks: ['gpu'],
+            minify: {
+                // 压缩HTML文件
+                removeComments: true, // 移除HTML中的注释
+                collapseWhitespace: true, // 删除空白符与换行符
+                minifyCSS: true // 压缩内联css
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'desktop_mcpu.html',
+            template: './src/other/desktop_mcpu.html',
+            hash: true,
+            favicon: path.resolve('favicon.ico'),
+            chunks: ['mcpu'],
+            minify: {
+                // 压缩HTML文件
+                removeComments: true, // 移除HTML中的注释
+                collapseWhitespace: true, // 删除空白符与换行符
+                minifyCSS: true // 压缩内联css
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'desktop_mgpu.html',
+            template: './src/other/desktop_mgpu.html',
+            hash: true,
+            favicon: path.resolve('favicon.ico'),
+            chunks: ['mgpu'],
+            minify: {
+                // 压缩HTML文件
+                removeComments: true, // 移除HTML中的注释
+                collapseWhitespace: true, // 删除空白符与换行符
+                minifyCSS: true // 压缩内联css
+            }
+        }),
     ]
 }
