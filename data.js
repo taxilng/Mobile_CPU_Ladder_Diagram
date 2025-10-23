2f748bb2c01a66374a2397026b928b09285ad4cd
tangxd5 <tangxd5@asiainfo.com>
2025-10-24 00:56:18 +0800

    桌面端CPU和GPU天梯图修改；增加骁龙8Elite Gen5, 天玑9500, A19, A19 Pro, 玄戒O1，麒麟8020

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var CPULIST = {
-  cpu: './img/CPU202508.webp',
-  gpu: './img/GPU202507.webp',
+  cpu: './img/CPU202509.webp',
+  gpu: './img/GPU202508.webp',
   mcpu: './img/mcpu202507.webp',
   mgpu: './img/mgpu202508.webp',
 }
